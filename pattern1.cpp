874621055ee4cf6f2ff29a08aa6e3455ed7c1cc8
anuragroy80948-gif <anuragroy80948@gmail.com>
2026-05-07 22:01:52 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+void pattern(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"enter the value of number";
+    cin>>n;
+    pattern(n);
+    return 0;
+}
+
+
